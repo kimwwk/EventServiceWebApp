35bf36361952b549b43d06ccc8b8c336b9360286
Kim Wong <15217035@life.hkbu.edu.hk>
2021-11-08 22:27:05 -0500

    test for deploying heroku

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const jsonServer = require("json-server");
 
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
+const middlewares = jsonServer.defaults();
+// const middlewares = jsonServer.defaults({
+//   static: "./build",
+// });
 
 const PORT = process.env.PORT || 8000;
 server.use(middlewares);
