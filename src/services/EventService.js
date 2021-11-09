ca1cf27527d100fd935076d5930a0ace375998a7
Kim Wong <15217035@life.hkbu.edu.hk>
2021-11-08 22:41:07 -0500

    test for deploying heroku

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import NProgress from "nprogress";
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: process.env.SERVER_PATH,
   withCredentials: false, // This is the default
   headers: {
     Accept: "application/json",
