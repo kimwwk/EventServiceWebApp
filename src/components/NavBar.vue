721962f255617c9c2dd2fd1eaca7eebab2b4f5ba
Kim Wong <15217035@life.hkbu.edu.hk>
2021-11-08 22:50:23 -0500

    remove comment

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="nav" class="nav">
-    <router-link to="/" class="brand">Real World Events</router-link> // this is
-    new
+    <router-link to="/" class="brand">Real World Events</router-link>
     <nav>
       <router-link :to="{ name: 'event-list' }">List</router-link> |
       <router-link :to="{ name: 'event-create' }">Create</router-link>
